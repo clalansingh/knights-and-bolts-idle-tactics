69895f42993716ed695186be86f7ee42a8a2e85f
Christopher Lalansingh <clalansingh@gmail.com>
2025-11-27 17:55:03 -0500

    Removing godot attempt

--- check_parse.gd
+++
@@ -1,9 +0,0 @@
-extends SceneTree
-
-const CombatManager = preload("res://scripts/combat_manager.gd")
-
-func _init():
-	print("Loaded CombatManager")
-	var cm = CombatManager.new()
-	print("Instantiated CombatManager")
-	quit()
