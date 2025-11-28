c6ff09aceb9fbea4db9ce5f569f659d941727887
Christopher Lalansingh <clalansingh@gmail.com>
2025-11-27 19:31:52 -0500

    Config updates

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,9 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  // REPLACE 'YOUR_REPO_NAME' with your actual GitHub repository name, e.g. '/knights-tactics/'
+  base: "/knights-and-bolts-idle-tactics/",
 })
