f0593009dcdf6b24763727ef266765d3660c9a72
Christopher Lalansingh <clalansingh@gmail.com>
2025-11-27 19:49:29 -0500

    CSS fix 1

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import React, { useState, useEffect, useRef } from 'react';
+import './App.css';
 import {
   Sword, Shield, Heart, Zap, Play, Pause, RefreshCw,
   ChevronRight, Skull, Activity, Lock, Settings, User,
